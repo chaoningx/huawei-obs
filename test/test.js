f97ccd7928ca2be9162e5d2b7e87d038b03c0c41
chaoningx <chaoningx@163.com>
2018-07-12 19:53:10 +0800

    add method headObject

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const path = require('path');
 const should = require('should');
 const OBS = require('../lib/index');
 
-const ORIGIN_FILE_PATH = '/Users/chaoningx/Desktop/test.flac';
+const ORIGIN_FILE_PATH = path.join(__dirname, 'resource', 'put_object.jpg');
 const OBJECT_NAME = `中国/北方/北京_test_${new Date().getTime()}.jpg`;
 const LOCAL_FILE_NAME = `北京_test_${new Date().getTime()}_download.jpg`;
 
@@ -40,6 +40,16 @@ describe('Huawei OBS', () => {
         should.exist(chunk);
         should.exist(processStatus);
         console.log(processStatus);
+      });
+    });
+  });
+
+  describe('#headObject()', () => {
+    it('should not return err just info', (done) => {
+      client.headObject(OBJECT_NAME, (err, info) => {
+        should.not.exist(err);
+        should.exist(info);
+        done();
       });
     });
   });
